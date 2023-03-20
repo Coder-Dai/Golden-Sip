1cdff57cf29280e13cf672dd744424babddeb8ca
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2023-03-20 20:15:44 +0000

    Create a basic hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import Nav from './components/Nav';
+import "./App.css";
+import React from "react";
+import { Nav } from "./components/Nav";
+import { HamburgerMenu } from "./components/HamburgerMenu";
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
+      <HamburgerMenu />
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import logo from '../assets/logo.png'
+import React from "react";
+import logo from "../assets/logo.png";
 
-const Nav = () => {
+export const Nav = () => {
   return (
     <header>
-        <img src={logo} alt="Golden Sip's Logo"></img>
-        <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Menu</li>
-            <li>Contact</li>
-        </ul>
+      <img src={logo} alt="Golden Sip's Logo"></img>
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
     </header>
-  )
-}
-
-export default Nav
+  );
+};
