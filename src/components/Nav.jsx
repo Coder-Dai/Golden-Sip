b1fbf3d2118e75b451aefeb60ff9343d66ec65e8
Tayamul <rai_tayamul@hotmail.com>
2023-03-20 18:31:54 +0000

    Add all links in the navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header>
+        <img></img>
+        <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Menu</li>
+            <li>Contact</li>
+        </ul>
+    </header>
   )
 }
 
