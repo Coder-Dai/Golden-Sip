fe24a8958343ee2f60a52b2199cefa1fe5a01eec
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2023-03-24 21:37:10 +0000

    Add 'Order now' button in the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,11 @@ export const Home = ({ isMenuOpen }) => {
       {!isMenuOpen && (
         <div className="home-container">
           <h1 className="title-text">Authentic Nepalese and Indian Cuisine</h1>
+          <button className="order-button-home-page">
+            <a href="https://www.just-eat.co.uk/restaurants-golden-sip-ha2/menu">
+              Order now
+            </a>
+          </button>
         </div>
       )}
     </div>
