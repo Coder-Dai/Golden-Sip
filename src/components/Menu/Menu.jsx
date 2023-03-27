30dcffe5e8205c1705e0fcce33791bff3d1d199d
Tayamul <rai_tayamul@hotmail.com>
2023-03-27 11:59:51 +0100

    Define a useState and ternary operator

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./menu.css";
 import {AiOutlineCaretUp} from 'react-icons/ai'
 import {AiOutlineCaretDown} from 'react-icons/ai'
 import vegS from './veg-s.json'
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <section id="menu">
       <h1>Menu</h1>
       <div className="menu-container">
         <div className="menu-content">
-          <button>
+          <button
+          onClick={() => setIsOpen((prev) => !prev)}>
             Veg-Starter
+            {!isOpen ? (
+              <AiOutlineCaretDown />)
+              :
+              (<AiOutlineCaretUp />
+            )}
           </button>
         </div>
         <div className="menu-content">
