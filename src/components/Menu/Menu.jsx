36652380ec91c7f8ddf3f76a328246261430ec4c
Tayamul <rai_tayamul@hotmail.com>
2023-03-27 13:42:10 +0100

    Design layout of the menu page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import "./menu.css";
+import {AiOutlineCaretUp} from 'react-icons/ai'
+import {AiOutlineCaretDown} from 'react-icons/ai'
+import vegS from './veg-s.json'
 
 const Menu = () => {
-  return <div>Menu</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <section id="menu">
+      <h1>Menu</h1>
+      <div className="menu-container">
+        <div className="menu-content">
+          <button
+          onClick={() => setIsOpen((prev) => !prev)}
+          className='menu-btn'>
+            Veg-Starter
+            {!isOpen ? (
+              <AiOutlineCaretDown />)
+              :
+              (<AiOutlineCaretUp />
+            )}
+          </button>
+
+        {isOpen && (
+          <div className="menu-title">
+            {vegS.map((item, i) => (
+              <div key={i} className='menu-list'>
+                <p className="menu-name">{item.name}</p>
+                <p className="menu-desc">{item.desc}</p>
+              </div>  
+            ))}
+          </div>  
+        )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Menu;
