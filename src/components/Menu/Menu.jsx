71002764c25c9ef877fca716141a1ac1eb29367d
Tayamul <rai_tayamul@hotmail.com>
2023-03-27 11:50:31 +0100

    Design a basic layout for the menu page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./menu.css";
+
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <section id="menu">
+      <h1>Menu</h1>
+      <div className="menu-container">
+        <div className="menu-content">
+          <button>Veg-Starter</button>
+        </div>
+        <div className="menu-content">
+          <button>Veg-Starter</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Menu;
