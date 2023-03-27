e0cb081942a6510d63a0caf27dda436206a4ef36
Tayamul <rai_tayamul@hotmail.com>
2023-03-27 09:33:40 +0100

    Import Menu component in the App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from "./components/Home/Home";
 import { HamburgerMenu } from "./components/HamburgerMenu/HamburgerMenu";
 import { About } from "./components/About/About";
 import { HamburgerNav } from "./components/HamburgerMenu/HamburgerNav";
+import Menu from "./components/Menu/Menu";
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -19,6 +20,7 @@ const App = () => {
       <HamburgerMenu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
       <Home isMenuOpen={isMenuOpen} />
       <About isMenuOpen={isMenuOpen} />
+      <Menu/>
     </div>
   );
 };
