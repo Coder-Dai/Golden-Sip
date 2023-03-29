7c1eb76c866d2cff448e36df732bbdced648cdba
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2023-03-29 19:24:17 +0100

    Fix navigation

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -5,12 +5,26 @@ import "./nav.css";
 export const Nav = () => {
   return (
     <nav className="nav-bar">
-      <a href='#home'><img src={logo} alt="Golden Sip's Logo" className="desktop-golden-sip-logo" /></a>
+      <a href="#home">
+        <img
+          src={logo}
+          alt="Golden Sip's Logo"
+          className="desktop-golden-sip-logo"
+        />
+      </a>
       <ul className="nav-links">
-        <a className="nav-lists" href='home'><li>Home</li></a>
-        <a className="nav-lists" href='about'><li>About us</li></a>
-        <a className="nav-lists" href='menu'><li>Menu</li></a>
-        <a className="nav-lists" href='contact'><li>Contact</li></a>
+        <a className="nav-lists" href="#home">
+          <li>Home</li>
+        </a>
+        <a className="nav-lists" href="#about">
+          <li>About us</li>
+        </a>
+        <a className="nav-lists" href="#menu">
+          <li>Menu</li>
+        </a>
+        <a className="nav-lists" href="#contact">
+          <li>Contact</li>
+        </a>
       </ul>
     </nav>
   );
