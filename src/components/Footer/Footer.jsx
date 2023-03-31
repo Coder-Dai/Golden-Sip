b5db08675ce1c45c069d0c7ee1d47ff1028ee577
Pratik Magar <67797986+magarpratik@users.noreply.github.com>
2023-03-31 18:38:35 +0100

    Use semantic tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import "./footer.css";
 
 export const Footer = ({ isMenuOpen }) => {
   return (
-    <div>
+    <>
       {!isMenuOpen && (
-        <section className="footer-container">
+        <footer className="footer-container">
           <p className="footer-text">Copyright 2023 All rights reserved</p>
-        </section>
+        </footer>
       )}
-    </div>
+    </>
   );
 };
