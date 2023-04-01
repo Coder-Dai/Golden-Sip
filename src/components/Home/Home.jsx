3104db4fe8082bb5ac3eff299769e6c34887b916
Tayamul <rai_tayamul@hotmail.com>
2023-04-01 14:24:03 +0100

    Add a target attribute to open a blank page when order button is clicked

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,8 @@ export const Home = ({ isMenuOpen }) => {
         <div className="home-container">
           <h1 className="title-text">Authentic Nepalese and Indian Cuisine</h1>
           <button className="order-button-home-page">
-            <a href="https://www.just-eat.co.uk/restaurants-golden-sip-ha2/menu">
+            <a href="https://www.just-eat.co.uk/restaurants-golden-sip-ha2/menu"
+            target='_blank' rel='noreferrer'>
               Order now
             </a>
           </button>
